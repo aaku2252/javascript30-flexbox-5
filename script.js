b267e6628d51cfb9007a63978087341d14cb4bf3
Akash <akkyjat601@gmail.com>
2021-04-11 11:45:58 +0530

    created final flexbox panel with added functions

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const panel = Array.from(document.querySelectorAll(" .panel"));
 
 for (let i = 0; i < panel.length; i++) {
-    // panel.forEach((x) => x.classList.remove("enlarge"));
-    // panel.forEach((x) => console.log(x));
     panel[i].addEventListener("click", () => {
+        panel.forEach((x) => {
+            x.classList.remove("enlarge");
+            let arr = Array.from(x.querySelectorAll("p"));
+            arr[0].classList.remove("text");
+            arr[2].classList.remove("text");
+        });
         panel[i].classList.toggle("enlarge");
         let parr = Array.from(panel[i].querySelectorAll("p"));
         parr[0].classList.toggle("text");
